8bfcf3976e505e4e6e61bf5745c176296d04c654
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-17 19:33:24 +0800

    feat:add some implemetion make the test shoule_throw_exception_when_save_given_null_case_name pass

--- src/main/java/com/tw/apistackbase/entity/LawCases.java
+++ src/main/java/com/tw/apistackbase/entity/LawCases.java
@@ -1,4 +1,42 @@
 package com.tw.apistackbase.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class LawCases {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false)
+    private String casename;
+    @Column(nullable = false)
+    private long caseHappenTime;
+
+    public LawCases() {
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCasename(String casename) {
+        this.casename = casename;
+    }
+
+    public void setCaseHappenTime(long caseHappenTime) {
+        this.caseHappenTime = caseHappenTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCasename() {
+        return casename;
+    }
+
+    public long getCaseHappenTime() {
+        return caseHappenTime;
+    }
+
 }
--- src/test/java/com/tw/apistackbase/repository/CasesRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/repository/CasesRepositoryTest.java
@@ -12,20 +12,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class CasesRepositoryTest {
+public class CasesRepositoryTest {
     private CasesRepository casesRepository;
-
+//AC1
     @Test
     public void shoule_throw_exception_when_save_given_null_case_name(){
         //given
-        LawCases cases=new LawCases();
+        LawCases lawCase1=new LawCases();
+        lawCase1.setCasename("case1");
         //when
         //then
         Assertions.assertThrows(Exception.class, new Executable() {
             @Override
             public void execute() throws Throwable {
-                casesRepository.save();
+                casesRepository.saveAndFlush(lawCase1);
             }
-        })
+        });
     }
 }
